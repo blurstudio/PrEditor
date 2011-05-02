43eab21da85a5161ce9513bc7bc5954de9e5886e
Mike Hendricks <mikeh@blur.com>
2011-05-02 21:48:44 +0000

    Douglas Lassance added create macro functionality to SoftimageCore.
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@11519 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- blurdev/cores/softimagecore.py
+++ blurdev/cores/softimagecore.py
@@ -14,6 +14,29 @@ from blurdev.cores.core import Core
 
 # -------------------------------------------------------------------------------------------------------------
 
+SOFTIMAGE_MACRO_TEMPLATE = """
+import win32com.client
+from win32com.client import constants
+
+def XSILoadPlugin( in_reg ):
+    in_reg.Author = "beta@blur.com"
+    in_reg.Name = "%(displayName)s"
+    in_reg.Major = 1
+    in_reg.Minor = 0
+    in_reg.RegisterCommand("%(displayName)s","%(displayName)s")
+    return True
+
+def %(displayName)s_Init( in_ctxt ):
+    oCmd = in_ctxt.Source
+    oCmd.Description = "%(tooltip)s"
+    oCmd.ReturnValue = False
+    return True
+
+def %(displayName)s_Execute(  ):
+    import blurdev
+    blurdev.runTool( "%(tool)s", "%(macro)s" )
+"""
+
 
 class SoftimageCore(Core):
     def __init__(self):
@@ -57,3 +80,40 @@ class SoftimageCore(Core):
         output = ToolType.Softimage | ToolType.LegacySoftimage
 
         return output
+
+    def createToolMacro(self, tool, macro=''):
+        """
+            \remarks	Overloads the createToolMacro virtual method from the Core class, this will create a command plugin for the
+                        Softimage application for the inputed Core tool
+            
+            \return		<bool> success
+        """
+        import win32com.client
+        from win32com.client import constants
+
+        # create the options for the tool macro to run
+        options = {
+            'tool': tool.objectName(),
+            'displayName': tool.displayName(),
+            'macro': macro,
+            'tooltip': tool.displayName(),
+            'id': str(tool.displayName()).replace(' ', '_').replace('::', '_'),
+        }
+
+        # create the macroscript
+        from PySoftimage import xsi
+
+        import os
+
+        pluginsPath = os.path.join(
+            xsi.GetInstallationPath2(constants.siUserPath), 'Application', 'Plugins'
+        )
+        filename = os.path.join(pluginsPath, options['displayName'] + '.py')
+        f = open(filename, 'w')
+        f.write(SOFTIMAGE_MACRO_TEMPLATE % options)
+        f.close()
+
+        # reloading softimage plugins
+        xsi.UpdatePlugins()
+
+        return True
