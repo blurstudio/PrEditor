f0f3582dd27270d51e65ba6b462c4a9693bb052c
Mike Hendricks <mikeh@blur.com>
2011-11-12 07:22:15 +0000

    Changed the flagHtml.
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@12391 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- blurdev/gui/widgets/pyularwidget/regexrefdialog.py
+++ blurdev/gui/widgets/pyularwidget/regexrefdialog.py
@@ -39,22 +39,22 @@ html = '''<table>
 
 flagsHtml = '''<table>
     <tr>
-        <td><b>I </b></td><td><b>Ignore Case </b></td><td>Perform case-insensitive matching; expressions like [A-Z] will match lowercase letters, too. This is not affected by the current locale.</td>
+        <td><b>I </b></td><td>Ignore Case  </td><td>Perform case-insensitive matching; expressions like [A-Z] will match lowercase letters, too. This is not affected by the current locale.</td>
     </tr>
     <tr>
-        <td><b>L </b></td><td><b>Locale</b></td><td>Make \w, \W, \b, \B, \s and \S dependent on the current locale.</td>
+        <td><b>L </b></td><td>Locale</td><td>Make \w, \W, \b, \B, \s and \S dependent on the current locale.</td>
     </tr>
     <tr>
-        <td><b>M </b></td><td><b>Multiline</b></td><td>When specified, the pattern character '^' matches at the beginning of the string and at the beginning of each line (immediately following each newline); and the pattern character '$' matches at the end of the string and at the end of each line (immediately preceding each newline). By default, '^' matches only at the beginning of the string, and '$' only at the end of the string and immediately before the newline (if any) at the end of the string.</td>
+        <td><b>M </b></td><td>Multiline</td><td>When specified, the pattern character '^' matches at the beginning of the string and at the beginning of each line (immediately following each newline); and the pattern character '$' matches at the end of the string and at the end of each line (immediately preceding each newline). By default, '^' matches only at the beginning of the string, and '$' only at the end of the string and immediately before the newline (if any) at the end of the string.</td>
     </tr>
     <tr>
-        <td><b>S </b></td><td><b>DOTALL</b></td><td>Make the '.' special character match any character at all, including a newline; without this flag, '.' will match anything except a newline.</td>
+        <td><b>S </b></td><td>DOTALL</td><td>Make the '.' special character match any character at all, including a newline; without this flag, '.' will match anything except a newline.</td>
     </tr>
     <tr>
-        <td><b>U </b></td><td><b>UNICODE</b></td><td>Make \w, \W, \b, \B, \d, \D, \s and \S dependent on the Unicode character properties database.</td>
+        <td><b>U </b></td><td>UNICODE</td><td>Make \w, \W, \b, \B, \d, \D, \s and \S dependent on the Unicode character properties database.</td>
     </tr>
     <tr>
-        <td><b>X </b></td><td><b>VERBOSE</b></td><td>This flag allows you to write regular expressions that look nicer. Whitespace within the pattern is ignored, except when in a character class or preceded by an unescaped backslash, and, when a line contains a '#' neither in a character class or preceded by an unescaped backslash, all characters from the leftmost such '#' through the end of the line are ignored.</td>
+        <td><b>X </b></td><td>VERBOSE</td><td>This flag allows you to write regular expressions that look nicer. Whitespace within the pattern is ignored, except when in a character class or preceded by an unescaped backslash, and, when a line contains a '#' neither in a character class or preceded by an unescaped backslash, all characters from the leftmost such '#' through the end of the line are ignored.</td>
     </tr>
 </table>
 '''
