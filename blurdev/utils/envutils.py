73d19ce5de757b92ce6c4dbfc7e1d7bf82791edc
Brendan Abel <brendana@blur.com>
2012-05-08 22:47:25 +0000

    added functions for getting logged in users.
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@13081 896c8824-d3f5-0310-9c61-ac502b9a79dc

---
+++ blurdev/utils/envutils.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+try:
+    import win32net
+    import win32netcon
+
+    WIN32 = True
+except ImportError:
+    WIN32 = False
+
+
+def username():
+    return os.environ.get('USERNAME', None)
+
+
+def hostname():
+    return os.environ.get('COMPUTERNAME', None)
+
+
+def currentUsernames():
+    """This gets a list of all usernames logged in across all windows sessions
+    on the current computer.
+    """
+    if not WIN32:
+        return []
+    server = os.environ.get('COMPUTERNAME')
+    res = 1
+    pref = win32netcon.MAX_PREFERRED_LENGTH
+    level = 0
+    total_list = set()
+    try:
+        user_list, total, res2 = win32net.NetWkstaUserEnum(server, level, res, pref)
+        for u in user_list:
+            total_list.add(u['username'])
+        return list(total_list)
+    except Exception:
+        return []
