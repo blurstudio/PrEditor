468912f2d45d99b10ce83c5e0115338dde5ee293
Mike Hendricks <mikeh@blur.com>
2012-05-25 02:09:26 +0000

    Created a DateWidget that allows you to not set a date. #8150
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@13156 896c8824-d3f5-0310-9c61-ac502b9a79dc

---
+++ blurdev/gui/widgets/clearabledateedit.py
@@ -0,0 +1,85 @@
+##
+#   \namespace  blurdev.gui.widgets.clearabledateedit
+#
+#   \remarks
+#
+#   \author     mikeh@blur.com
+#   \author     Blur Studio
+#   \date       05/24/12
+#
+
+import blurdev
+from PyQt4.QtCore import Qt, QDate
+from PyQt4.QtGui import (
+    QDateEdit,
+    QToolButton,
+    QStyle,
+    QStyleOptionSpinBox,
+    QIcon,
+    QLineEdit,
+)
+
+
+class ClearableDateEdit(QDateEdit):
+    def __init__(self, parent):
+        super(ClearableDateEdit, self).__init__(parent)
+        self.uiClearBTN = QToolButton(self)
+        self.uiClearBTN.setText('No')
+        self.uiClearBTN.setIcon(QIcon(blurdev.resourcePath('img/cancel.png')))
+        self.uiClearBTN.resize(20, 20)
+        self.uiClearBTN.setCheckable(True)
+        self.uiClearBTN.setChecked(True)
+        self.uiClearBTN.setToolTip('Remove date')
+        self.uiClearBTN.clicked.connect(self.clearClicked)
+        self.uiNoDateLBL = QLineEdit(self)
+        self.uiNoDateLBL.setText('No Date')
+        self.uiNoDateLBL.setReadOnly(False)
+        self.uiNoDateLBL.setVisible(False)
+        self.uiNoDateLBL.setFocusPolicy(Qt.NoFocus)
+        frameWidth = self.style().pixelMetric(QStyle.PM_DefaultFrameWidth)
+
+    def clear(self):
+        self.uiClearBTN.setChecked(False)
+        self.clearClicked(False)
+
+    def clearClicked(self, state):
+        self.lineEdit().setEnabled(state)
+        self.lineEdit().setVisible(state)
+        self.uiNoDateLBL.setVisible(not state)
+
+    def date(self):
+        if self.uiClearBTN.isChecked():
+            return super(ClearableDateEdit, self).date()
+        return QDate()
+
+    def keyPressEvent(self, event):
+        if self.uiClearBTN.isChecked():
+            super(ClearableDateEdit, self).keyPressEvent(event)
+
+    def isCleared(self):
+        return self.uiClearBTN.isChecked()
+
+    def mousePressEvent(self, event):
+        if self.uiClearBTN.isChecked():
+            super(ClearableDateEdit, self).mousePressEvent(event)
+
+    def resizeEvent(self, event):
+        opt = QStyleOptionSpinBox()
+        opt.rect = self.rect()
+        cbWidth = self.uiClearBTN.size().width()
+        self.uiClearBTN.resize(cbWidth, opt.rect.height())
+        opt.rect.setX(cbWidth)
+        combo = self.style().subControlRect(
+            QStyle.CC_SpinBox, opt, QStyle.SC_SpinBoxEditField, self
+        )
+        self.lineEdit().setGeometry(combo)
+        self.uiNoDateLBL.setGeometry(combo)
+
+    def setDate(self, date):
+        super(ClearableDateEdit, self).setDate(date)
+        self.uiClearBTN.setChecked(date.isValid())
+        self.clearClicked(date.isValid())
+
+    def wheelEvent(self, event):
+        if self.uiClearBTN.isChecked():
+            super(ClearableDateEdit, self).wheelEvent(event)
