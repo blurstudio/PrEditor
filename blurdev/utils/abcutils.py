20cd162868c0009ee7797ac60669e8fefa882318
Brendan Abel <brendana@blur.com>
2012-05-30 19:41:26 +0000

    update to abcutils to support compound properties that were added in exocortex alembic crate 1.0.68
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@13176 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- blurdev/utils/abcutils.py
+++ blurdev/utils/abcutils.py
@@ -2,6 +2,7 @@ import re
 import sys
 import os
 from pprint import pprint
+import logging
 
 # set the exocortex license server env var
 os.environ["EXOCORTEX_LICENSE"] = "5053@mandarin004"
@@ -25,6 +26,9 @@ def readArchive(src):
 
 
 class ABCArchive(object):
+
+    indent = "  "
+
     def __init__(self):
         super(ABCArchive, self).__init__()
         self.filename = None
@@ -32,13 +36,14 @@ class ABCArchive(object):
         self.sampletimes = None
         self.objects = None
 
-    def toString(self):
+    def toString(self, indent=""):
         txt = []
         txt.append('ARCHIVE: %s' % self.filename)
         obj_names = sorted(self.objects.keys())
+        indent += ABCArchive.indent
         for obj_name in obj_names:
             obj = self.objects[obj_name]
-            txt.append(obj.toString())
+            txt.append(obj.toString(indent))
         return '\n'.join(txt)
 
     def getIdentifiers(self):
@@ -80,40 +85,44 @@ class ABCArchive(object):
         return archive
 
     def toFile(self, filename):
+        logging.debug("Writing oArchive to file: %s" % filename)
         oarchive = alembic.getOArchive(filename)
-        errors = []
-        try:
-            if self.sampletimes:
-                # TODO: not sure what the 2d array is?
-                oarchive.createTimeSampling(self.sampletimes[1])
+        if self.sampletimes:
+            # TODO: not sure what the 2d array is?
+            oarchive.createTimeSampling(self.sampletimes[1])
 
-            oids = self.objects.keys()
-            oids.sort()
-            for oid in oids:
-                obj = self.objects[oid]
-                try:
-                    oobj = oarchive.createObject(obj.type, obj.id)
-                except alembic.error:
-                    errors.append('OBJECT: %s, %s' % (obj.type, obj.id))
+        def _copy_property(prop, oprop):
+            for i in range(0, prop.nbstoredsamples):
+                vals = prop.values[i]
+                oprop.setValues(vals)
+
+        def _copy_compound_property(prop, oprop):
+            for sub_prop in prop.properties.values():
+                if sub_prop.compound:
+                    _copy_compound_property(
+                        sub_prop, oprop.getProperty(sub_prop.name, sub_prop.type)
+                    )
                 else:
-                    if obj.metadata:
-                        # TODO: might need to pad metadata
-                        oobj.setMetaData(obj.metadata)
-                    if obj.properties:
-                        for prop in obj.properties.values():
-                            try:
-                                oprop = oobj.getProperty(prop.name, prop.type)
-                            except alembic.error:
-                                errors.append(
-                                    'OBJECT: %s, %s ||| PROP: %s, %s'
-                                    % (obj.type, obj.id, prop.type, prop.name)
-                                )
-                            else:
-                                oprop.setValues(prop.values)
-        finally:
-            oarchive = None
-        print 'ERRORS'
-        pprint(errors)
+                    _copy_property(
+                        sub_prop, oprop.getProperty(sub_prop.name, sub_prop.type)
+                    )
+
+        oids = self.objects.keys()
+        oids.sort()
+        for oid in oids:
+            obj = self.objects[oid]
+            oobj = oarchive.createObject(obj.type, obj.id)
+            if obj.metadata:
+                # TODO: might need to pad metadata
+                oobj.setMetaData(obj.metadata)
+            if obj.properties:
+                for prop in obj.properties.values():
+                    oprop = oobj.getProperty(prop.name, prop.type)
+                    if prop.compound:
+                        _copy_compound_property(prop, oprop)
+                    else:
+                        _copy_property(prop, oprop)
+        oarchive = None
 
 
 class ABCObject(object):
@@ -127,13 +136,15 @@ class ABCObject(object):
         self.properties = None
         self.abcarchive = None
 
-    def toString(self):
+    def toString(self, indent=""):
         txt = []
-        txt.append('    %s (%s)' % (self.id, self.type))
+        txt.append('%s%s (%s)' % (indent, self.id, self.type))
+        txt.append('%s--metadata: %s' % (indent, str(self.metadata)))
+        indent += ABCArchive.indent
         propnames = sorted(self.properties.keys())
         for propname in propnames:
             prop = self.properties[propname]
-            txt.append(prop.toString())
+            txt.append(prop.toString(indent))
         return '\n'.join(txt)
 
     @classmethod
@@ -163,14 +174,27 @@ class ABCProperty(object):
         self.values = None
         self.abcobject = None
         self.abcarchive = None
+        self.compound = None
+        self.properties = None
 
-    def toString(self):
+    def toString(self, indent=""):
         txt = []
-        txt.append('        %s (%s)' % (self.name, self.type))
+        txt.append('%s%s (%s)' % (indent, self.name, self.type))
+        if self.compound:
+            indent += ABCArchive.indent
+            propnames = sorted(self.properties.keys())
+            for propname in propnames:
+                prop = self.properties[propname]
+                txt.append(prop.toString(indent))
+        else:
+            txt.append('%s--size: %s' % (indent, str(self.size)))
+            for i in range(self.nbstoredsamples):
+                valtxt = '%s--values(%s): %s' % (indent, i, self.values[i])
+                txt.append(valtxt)
         return '\n'.join(txt)
 
     @classmethod
-    def from_iProperty(cls, iprop, abcobject=None):
+    def from_iProperty(cls, iprop, abcobject=None, abcproperty=None):
         prop = cls()
         prop.abcobject = abcobject
         if abcobject is not None:
@@ -179,10 +203,17 @@ class ABCProperty(object):
         prop.type = iprop.getType()
         prop.sampletimes = iprop.getSampleTimes()
         prop.nbstoredsamples = iprop.getNbStoredSamples()
-        try:
-            prop.size = iprop.getSize()
-            prop.values = iprop.getValues()
-        except alembic.error:
-            pass
+        prop.size = iprop.getSize()
+        prop.values = []
+        for i in range(prop.nbstoredsamples):
+            vals = iprop.getValues(i)
+            prop.values.append(vals)
 
+        prop.compound = iprop.isCompound()
+        if prop.compound:
+            prop.properties = {}
+            for sub_iprop_name in iprop.getPropertyNames():
+                sub_iprop = iprop.getProperty(sub_iprop_name)
+                sub_prop = ABCProperty.from_iProperty(sub_iprop, abcobject, prop)
+                prop.properties[sub_iprop_name] = sub_prop
         return prop
