520a29ffe6e3f786d959be5b1c6cb38b50309a24
Brendan Abel <brendana@blur.com>
2012-06-29 00:33:35 +0000

    added fume utils library
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@13347 896c8824-d3f5-0310-9c61-ac502b9a79dc

---
+++ blurdev/utils/fumeutils.py
@@ -0,0 +1,94 @@
+import sys
+import struct
+
+
+def readFXD(filename):
+    return FXDFile.read(filename)
+
+
+class FXDFile(object):
+
+    ChannelTypes = enum(
+        "None", "Fuel", "Temp", "Smoke", "Fluid", "Velocity", "Voxel", "Color"
+    )
+
+    def __init__(self):
+        super(FXDFile, self).__init__()
+        self.filename = ''
+        self.attributes = {}
+
+    @classmethod
+    def read(cls, filename):
+        fxd = FXDFile()
+        fxd.filename = filename
+
+        sizeof_ushort = 2
+        sizeof_int = 4
+        sizeof_float = 4
+        sizeof_bool = 1
+
+        fmt_ushort = 'H'
+        fmt_int = 'i'
+        fmt_float = 'f'
+        fmt_bool = '?'
+
+        f = open(filename, 'rb')
+        (outputFileVer,) = struct.unpack(fmt_ushort, f.read(sizeof_ushort))
+        (step,) = struct.unpack(fmt_int, f.read(sizeof_int))
+        (fileScale,) = struct.unpack(fmt_float, f.read(sizeof_float))
+        (dx,) = struct.unpack(fmt_float, f.read(sizeof_float))
+
+        (lx,) = struct.unpack(fmt_float, f.read(sizeof_float))
+        (ly,) = struct.unpack(fmt_float, f.read(sizeof_float))
+        (lz,) = struct.unpack(fmt_float, f.read(sizeof_float))
+
+        (lxmax,) = struct.unpack(fmt_float, f.read(sizeof_float))
+        (lymax,) = struct.unpack(fmt_float, f.read(sizeof_float))
+        (lzmax,) = struct.unpack(fmt_float, f.read(sizeof_float))
+
+        (nx,) = struct.unpack(fmt_float, f.read(sizeof_float))
+        (ny,) = struct.unpack(fmt_float, f.read(sizeof_float))
+        (nz,) = struct.unpack(fmt_float, f.read(sizeof_float))
+
+        (nx0,) = struct.unpack(fmt_float, f.read(sizeof_float))
+        (ny0,) = struct.unpack(fmt_float, f.read(sizeof_float))
+        (nz0,) = struct.unpack(fmt_float, f.read(sizeof_float))
+
+        (nxmax,) = struct.unpack(fmt_float, f.read(sizeof_float))
+        (nymax,) = struct.unpack(fmt_float, f.read(sizeof_float))
+        (nzmax,) = struct.unpack(fmt_float, f.read(sizeof_float))
+
+        # struct boolean reading isn't supported in 2.4 (is in 2.6)
+        # 	adaptive, = struct.unpack(fmt_bool, f.read(sizeof_bool))
+        adaptive = bool(ord(f.read(sizeof_bool)))
+
+        (outputvars,) = struct.unpack(fmt_int, f.read(sizeof_int))
+        outvars = [vars.toString(v) for v in vars.values() if int(str(outputvars)) & v]
+
+        f.close()
+
+        attributes = fxd.attributes
+        attributes['outputFileVer'] = outputFileVer
+        attributes['step'] = step
+        attributes['fileScale'] = fileScale
+        attributes['dx'] = dx
+        attributes['lx'] = lx
+        attributes['ly'] = ly
+        attributes['lz'] = lz
+        attributes['lxmax'] = lxmax
+        attributes['lymax'] = lymax
+        attributes['lzmax'] = lzmax
+        attributes['nx'] = nx
+        attributes['ny'] = ny
+        attributes['nz'] = nz
+        attributes['nx0'] = nx0
+        attributes['ny0'] = ny0
+        attributes['nz0'] = nz0
+        attributes['nxmax'] = nxmax
+        attributes['nymax'] = nymax
+        attributes['nzmax'] = nzmax
+        attributes['adaptive'] = adaptive
+        attributes['outputvars'] = outputvars
+        attributes['channelTypes'] = outvars
+
+        return fxd
