fd7fa419db76be1ff56b4dbffcf1a8fbdf33a7b1
Mike Hendricks <mikeh@blur.com>
2012-09-20 01:21:25 +0000

    command prompt no longer shows up when extractVideoFrame is run. Per Jeremy.
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@13614 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- blurdev/media/functions.py
+++ blurdev/media/functions.py
@@ -37,7 +37,13 @@ def extractVideoFrame(filename, outputpath):
         options['ffmpeg'] = options['ffmpeg'].replace(' (x86)', '')
     options['output'] = outputpath
     cmd = '"%(ffmpeg)s" -i "%(source)s" -t 1 -f image2 "%(output)s"' % options
-    out = subprocess.Popen(cmd)
+    out = subprocess.Popen(
+        cmd,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        stdin=subprocess.PIPE,
+    )
     out.wait()
     return os.path.exists(outputpath)
 
