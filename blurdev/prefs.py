blurstudio/PrEditor

commit 6f1d5fc0deced3a3c7f94d77c301292c596a8098

Consistent line endings. Use os.path.join instead of hardcoded backslash.
git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@13960 896c8824-d3f5-0310-9c61-ac502b9a79dc

blurdev/prefs.py
@@ -65,7 +65,7 @@ class Preference(XMLDocument):
             }
         else:
             path = osystem.expandvars(os.environ['BDEV_PATH_PREFS'])
-        return os.path.join(path, 'app_%s\\' % coreName)
+        return os.path.join(path, 'app_%s' % coreName)
 
     def recordProperty(self, key, value):
         """ connects to the root recordProperty method """
@@ -159,7 +159,7 @@ def find(name, reload=False, coreName='', shared=False, index=0):
         pref.setShared(shared)
         pref.setCoreName(coreName)
         # look for a default preference file
-        filename = pref.path(coreName, shared) + '%s.pref' % key
+        filename = os.path.join(pref.path(coreName, shared), '%s.pref' % key)
         success = False
         if os.path.exists(filename):
             success = pref.load(filename)

commit 4a4cd32913ad38ff567cc5b268d3138a08c9b871

Fixed the filename generation that was broken by r13960. The hardcoded backslash was needed because of the way the filename was generated.
git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@13977 896c8824-d3f5-0310-9c61-ac502b9a79dc

blurdev/prefs.py
@@ -46,9 +46,8 @@ class Preference(XMLDocument):
         """ return this documents filename, deriving the default filename from its name and standard preference location  """
         if not self._filename:
             key = self.name().lower().replace(' ', '-')
-            self._filename = (
-                self.path(coreName=self._coreName, shared=self._shared)
-                + '%s.pref' % key
+            self._filename = os.path.join(
+                self.path(coreName=self._coreName, shared=self._shared), '%s.pref' % key
             )
         return self._filename
 