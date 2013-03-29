95c5dc89226c5968e518c91166de0d1f7dfda2f8
Mike Hendricks <mikeh@blur.com>
2013-03-29 23:46:24 +0000

    Added support for paste events, and fixed problem where QMimeData.html would replace the text in the widget.
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@14439 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- blurdev/gui/widgets/dragdroptestwidget.py
+++ blurdev/gui/widgets/dragdroptestwidget.py
@@ -8,38 +8,45 @@
 # 	:date		07/09/10
 #
 
-from PyQt4.QtGui import QTextEdit
-from PyQt4.QtCore import QString, Qt
+from PyQt4.QtGui import QTextEdit, QDropEvent
+from PyQt4.QtCore import QString, Qt, QMimeData
+from xml.sax.saxutils import escape
 
 
 class DragDropTestWidget(QTextEdit):
+    def insertFromMimeData(self, mimeData):
+        self.logEvent(mimeData)
+
     def logEvent(self, event):
         html = []
-        html.append(
-            '<h1>Drop Event</h1><small><b>source:</b></small> %s' % event.source()
-        )
-        html.append(
-            '<small><b>proposed action:</b></small> %s' % event.proposedAction()
-        )
-        possibleActions = event.possibleActions().__int__()
-        html.append('<small><b>possible actions:</b></small> %i' % possibleActions)
-        actions = {
-            Qt.CopyAction: 'Copy',
-            Qt.MoveAction: 'Move',
-            Qt.LinkAction: 'Link',
-            Qt.ActionMask: 'action Mask',
-            Qt.IgnoreAction: 'ignore',
-            Qt.TargetMoveAction: 'target move action',
-        }
-        for key in sorted(actions.keys()):
-            if possibleActions & key and key <= possibleActions:
-                html.append(
-                    '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small><b>%s</b></small> %i'
-                    % (actions[key], key)
-                )
-
+        if isinstance(event, QDropEvent):
+            html.append(
+                '<h1>Drop Event</h1><small><b>source:</b></small> %s' % event.source()
+            )
+            html.append(
+                '<small><b>proposed action:</b></small> %s' % event.proposedAction()
+            )
+            possibleActions = event.possibleActions().__int__()
+            html.append('<small><b>possible actions:</b></small> %i' % possibleActions)
+            actions = {
+                Qt.CopyAction: 'Copy',
+                Qt.MoveAction: 'Move',
+                Qt.LinkAction: 'Link',
+                Qt.ActionMask: 'action Mask',
+                Qt.IgnoreAction: 'ignore',
+                Qt.TargetMoveAction: 'target move action',
+            }
+            for key in sorted(actions.keys()):
+                if possibleActions & key and key <= possibleActions:
+                    html.append(
+                        '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<small><b>%s</b></small> %i'
+                        % (actions[key], key)
+                    )
+            data = event.mimeData()
+        elif isinstance(event, QMimeData):
+            html.append('<h1>insertFromMimeData Event</h1><small><b></b></small>')
+            data = event
         # Mime Data
-        data = event.mimeData()
         html.append(
             '<hr><h1>Mime Data</h1><small><b>has color:</b></small> %s'
             % data.hasColor()
@@ -49,11 +56,11 @@ class DragDropTestWidget(QTextEdit):
         html.append('<small><b>has text:</b></small> %s' % data.hasText())
         html.append('<small><b>has urls:</b></small> %s' % data.hasUrls())
         html.append('<br>')
-        html.append('<small><b>text:</b></small> %s' % str(data.text()))
-        html.append('<small><b>html:</b></small> %s' % str(data.html()))
+        html.append('<small><b>text:</b></small> %s' % unicode(data.text()))
+        html.append('<small><b>html:</b></small> %s' % escape(unicode(data.html())))
         html.append(
             '<small><b>urls:</b></small><br> %s'
-            % '<br>'.join([str(url.toString()) for url in data.urls()])
+            % '<br>'.join([unicode(url.toString()) for url in data.urls()])
         )
         html.append('<small><b>Additional Formats:</b></small>')
         excludeFormats = set(
