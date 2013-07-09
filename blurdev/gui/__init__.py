9a20ba1950f71f296e0b4ebd3ebba557ad3abdad
Mike Hendricks <mikeh@blur.com>
2013-07-09 01:19:04 +0000

    Added a function that allows you to comp multiple pixmaps together into a single pixmap. #12536
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@14686 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- blurdev/gui/__init__.py
+++ blurdev/gui/__init__.py
@@ -13,6 +13,49 @@ from dialog import Dialog
 from wizard import Wizard
 
 
+def compPixmap(imageData):
+    """
+    Composits the given pixmaps or image paths into a single pixmap. It takes a list of lists containing a image path or a pixmap and
+    optionaly a list of cordinate data. The cordinate data can be just position [5, 5] or position and size [5, 5, 10, 10]. The first
+    item in the list becomes the base canvas being drawn on and ignores the cordinate data.
+
+    Example of two step compositing::
+    data = [[trax.gui.findIconFile(r'employeeReview\1')],
+            [trax.gui.findIconFile(r'employeeReview\alert'), [5, 5]]]
+    map = buildPixmap(data)
+    data = [[trax.gui.findIconFile(r'employeeReview\blank')], 
+            [map, [4,2]]]
+    map = buildPixmap(data)
+    """
+    from PyQt4.QtGui import QPainter, QPixmap
+
+    if isinstance(imageData[0][0], QPixmap):
+        map = imageData[0][0]
+    else:
+        map = QPixmap(imageData[0][0])
+    imageData.pop(0)
+    painter = QPainter()
+    painter.begin(map)
+    for data in imageData:
+        if isinstance(data[0], QPixmap):
+            overlay = data[0]
+        else:
+            overlay = QPixmap(data[0])
+        rect = map.rect()
+        oRect = overlay.rect()
+        rect.setSize(oRect.size())
+        if len(data) > 1:
+            rect.moveTo(data[1][0], data[1][1])
+            if len(data[1]) > 2:
+                print rect
+                rect.setWidth(data[1][2])
+                rect.setHeight(data[1][3])
+                print '-', rect
+        painter.drawPixmap(rect, overlay, oRect)
+    painter.end()
+    return map
+
+
 def loadUi(filename, widget, uiname=''):
     """
         \remarks	use's Qt's uic loader to load dynamic interafces onto the inputed widget
