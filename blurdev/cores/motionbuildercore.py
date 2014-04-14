61421579983ce2ffcafacc8b87e09eb055992c2d
Mike Hendricks <mikeh@blur.com>
2014-04-14 18:40:40 +0000

    Motion Builder core now shutdowns when Motion Builder closes. It also controls creating the Blur menu inside Motion Builder. It also register's the proper path in os.environ['path'] to load any Qt dlls that are not already loaded. #12325
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@15634 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- blurdev/cores/motionbuildercore.py
+++ blurdev/cores/motionbuildercore.py
@@ -1,8 +1,19 @@
+import os
+import sys
 from PyQt4.QtGui import QApplication
 import pyfbsdk
 
 import blurdev.tools.tool
 from blurdev.cores.core import Core
+
+# >>> os.path.abspath(os.curdir)
+# C:\Program Files\Autodesk\MotionBuilder 2014
+# Because the python working directory is the root of the motion builder install
+# we have to explicitly add the qt dll paths so any dll's that were not loaded by
+# qt itself are properly found when loaded later when they are dynamically loaded.
+os.environ['path'] = ';'.join(
+    (os.path.split(sys.executable)[0], os.environ.get('path', ''))
+)
 
 
 class MotionBuilderCore(Core):
@@ -13,6 +24,9 @@ class MotionBuilderCore(Core):
     def __init__(self, *args, **kargs):
         kargs['objectName'] = 'motionbuilder'
         Core.__init__(self, *args, **kargs)
+        # Shutdown blurdev when Motion builder closes
+        if QApplication.instance():
+            QApplication.instance().aboutToQuit.connect(self.shutdown)
 
     def activeWindow(self):
         """
@@ -24,6 +38,59 @@ class MotionBuilderCore(Core):
             while window.parent():
                 window = window.parent()
         return window
+
+    def createSystemMenu(self):
+        """
+        Builds our menu for motion builder to launch treegrunt, logger, etc.
+        """
+
+        def eventMenu(control, event):
+            name = event.Name
+            import blurdev
+
+            if name == "Treegrunt":
+                blurdev.core.showTreegrunt()
+            elif name == "New Script":
+                blurdev.core.newScript()
+            elif name == "Open Script":
+                blurdev.core.openScript()
+            elif name == "Run Script":
+                blurdev.core.runScript()
+            elif name == "Show IDE":
+                blurdev.core.showIdeEditor()
+            elif name == "Python Logger":
+                blurdev.core.showLogger()
+            elif name == "Show Toolbar":
+                blurdev.core.showToolbar()
+            elif name == "Show Lovebar":
+                blurdev.core.showLovebar()
+
+        mgr = pyfbsdk.FBMenuManager()
+        blurMenu = mgr.GetMenu('Blur')
+        if blurMenu:
+            # remove all menus
+            item = blurMenu.GetFirstItem()
+            while item:
+                blurMenu.DeleteItem(item)
+                item = blurMenu.GetFirstItem()
+        else:
+            # create the menu
+            blurMenu = mgr.InsertBefore(None, 'Help', 'Blur').Menu
+        blurMenu.OnMenuActivate.Add(eventMenu)
+        mgr.InsertLast('Blur', 'Treegrunt')
+        # -------
+        mgr.InsertLast('Blur', '')  # Seperator
+        mgr.InsertLast('Blur', 'New Script')
+        mgr.InsertLast('Blur', 'Open Script')
+        mgr.InsertLast('Blur', 'Run Script')
+        # -------
+        mgr.InsertLast('Blur', '')  # Seperator
+        mgr.InsertLast('Blur', 'Show IDE')
+        mgr.InsertLast('Blur', 'Python Logger')
+        # -------
+        mgr.InsertLast('Blur', '')  # Seperator
+        mgr.InsertLast('Blur', 'Show Toolbar')
+        mgr.InsertLast('Blur', 'Show Lovebar')
 
     def macroName(self):
         """
