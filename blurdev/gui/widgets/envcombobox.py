4082cae9e1ef7c47604bc039613449588b14f521
Mike Hendricks <mikeh@blur.com>
2014-12-13 01:10:05 +0000

    Created a QComboBox allowing you to style a item with specific text differently than the other items in the popup. Treegrunt now uses this EnvComboBox to show the default treegrunt environment. It fixes alot of problems with the old implementation, and should be fairly easy to style using stylesheets. Removed most of the QSlider customization that breaks slider tick display.
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@16227 896c8824-d3f5-0310-9c61-ac502b9a79dc

---
+++ blurdev/gui/widgets/envcombobox.py
@@ -0,0 +1,119 @@
+from PyQt4.QtGui import QComboBox, QColor, QBrush, QStyledItemDelegate
+from PyQt4.QtCore import pyqtProperty, Qt
+
+
+class EnvComboBoxDelegate(QStyledItemDelegate):
+    def paint(self, painter, option, index):
+        parent = self.parent()
+        if parent and index.data(parent.DefaultEnvRole).toPyObject() == True:
+            self.initStyleOption(option, index)
+            brush = index.data(Qt.BackgroundRole).toPyObject()
+            if not brush:
+                brush = QBrush()
+            brush.setColor(parent.defaultEnvBackground)
+            brush.setStyle(Qt.SolidPattern)
+            option.backgroundBrush = brush
+            paletteColor = option.palette.color(option.palette.Text)
+            option.palette.setColor(option.palette.Text, parent.defaultEnvColor)
+            style = parent.style()  # QApplication.instance().style()
+            style.drawControl(style.CE_ItemViewItem, option, painter)
+            option.palette.setColor(option.palette.Text, paletteColor)
+        else:
+            super(EnvComboBoxDelegate, self).paint(painter, option, index)
+
+
+class EnvComboBox(QComboBox):
+    """ QComboBox that uses stylesheets to control its color and the color of its popup items.
+    
+    Here is a example stylesheet:
+        QComboBox#uiEnvironmentDDL, QComboBox#uiEnvironmentDDL QListView::item {
+            /* Default color of the default items in the popup via the "QComboBox#uiEnvironmentDDL" selector */
+            qproperty-defaultEnvBackground: rgb(0, 200, 0, 150);
+            qproperty-defaultEnvColor: black;
+            /* All other popup items are styled via the "QComboBox#uiEnvironmentDDL QListView::item" 
+            selector. It is also used to style the combo box via the "QComboBox#uiEnvironmentDDL" selector */
+            background: rgb(200, 131, 0, 150);
+        }
+        QComboBox#uiEnvironmentDDL QListView::item:selected {
+            /* Style the selected item in the popup */
+            background: rgb(0, 0, 255);
+        }
+        /* Used to style the background color of the combo box when using the defaultEnv */
+        QComboBox#uiEnvironmentDDL[defaultActive="true"] {
+            background: rgb(0, 200, 0, 150);
+        }
+    
+    Attributes:
+        defaultEnvColor: The color to render the default environment item text with.
+        defaultEnvBackground: The background color for the default environment item.
+    """
+
+    DefaultEnvRole = Qt.UserRole + 1
+
+    def __init__(self, parent=None):
+        super(EnvComboBox, self).__init__(parent)
+        self._defaultEnvColor = QColor(Qt.black)
+        self._defaultEnvBackground = QColor(0, 200, 0, 50)
+        self._defaultActive = False
+        self.setItemDelegate(EnvComboBoxDelegate(self))
+        self.currentIndexChanged.connect(self.updateColors)
+
+    def defaultEnv(self):
+        model = self.model()
+        for row in range(model.rowCount()):
+            index = model.index(row, 0)
+            if index.data(self.DefaultEnvRole).toPyObject() == True:
+                return index.data(Qt.DisplayRole).toPyObject()
+        return None
+
+    def setDefaultEnv(self, name):
+        """ Marks all items in the model using the provided text as defaultEnv
+        
+        This allows more than one environment to be marked as default as long as it has the same
+        name. You take care to not provide two environments with the same name in the combo box as
+        the defaultEnv method only returns the text of the first item marked as default.
+        
+        Args:
+            name(str): The text of the item you want to mark as default
+        """
+        model = self.model()
+        for row in range(model.rowCount()):
+            index = model.index(row, 0)
+            if index.data(Qt.DisplayRole).toPyObject() == name:
+                model.setData(index, True, self.DefaultEnvRole)
+            else:
+                model.setData(index, False, self.DefaultEnvRole)
+        self.updateColors()
+
+    def updateColors(self):
+        """ Update the defaultActive property and force the stylesheet to refresh """
+        self.defaultActive = self.defaultEnv() == self.currentText()
+        # Force the styleSheet to re-evaluate so it sees the new defaultActive state.
+        sheet = self.styleSheet()
+        if not sheet:
+            self.setStyleSheet('/**/')
+        self.setStyleSheet(sheet)
+
+    @pyqtProperty(bool)
+    def defaultActive(self):
+        return self._defaultActive
+
+    @defaultActive.setter
+    def defaultActive(self, color):
+        self._defaultActive = color
+
+    @pyqtProperty(QColor)
+    def defaultEnvColor(self):
+        return self._defaultEnvColor
+
+    @defaultEnvColor.setter
+    def defaultEnvColor(self, color):
+        self._defaultEnvColor = color
+
+    @pyqtProperty(QColor)
+    def defaultEnvBackground(self):
+        return self._defaultEnvBackground
+
+    @defaultEnvBackground.setter
+    def defaultEnvBackground(self, color):
+        self._defaultEnvBackground = color
