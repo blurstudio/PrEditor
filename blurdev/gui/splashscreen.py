7a874e941be50e402755967fcf91d739ba8aed2d
Mike Hendricks <mikeh@blur.com>
2016-04-07 17:45:29 +0000

    blurdev.gui.splashscreen's environment variables are created using the standard resource.settings option.
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@17825 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- blurdev/gui/splashscreen.py
+++ blurdev/gui/splashscreen.py
@@ -5,24 +5,30 @@ import random
 from PyQt4.QtGui import QPixmap, QSplashScreen
 from PyQt4.QtCore import Qt
 
-# Temporary workaround for providing default environment variable for splashscreen source dir.
-import platform
-
-if platform.uname()[0] in [
-    'Linux',
-]:
-    SPLASH_ROOT_DEFAULT = '/mnt/dev/dev/share_all/splash'
-else:
-    SPLASH_ROOT_DEFAULT = r'\\source\source\dev\share_all\splash'
+SPLASH_DIR = os.environ.get('BDEV_SPLASHSCREEN_SOURCE_DIR')
 
 
-SPLASH_DIR = os.environ.get('BDEV_SPLASHSCREEN_SOURCE_DIR', SPLASH_ROOT_DEFAULT)
-
-
-def randomSplashScreen(toolname='default', minsize=128):
+def randomSplashScreen(toolname='default', minsize=128, allowDefault=True):
+    """ Randomly picks a image for the provided toolname and returns a QSplashScreen or None.
+    
+    The QSplashScreen will have show() called on it when it is returned.
+    
+    Args:
+        toolname (str): The name of a sub-directory of the directory defined in 
+            BDEV_SPLASHSCREEN_SOURCE_DIR. If this directory does not exist, it will 
+            use the default directory. Defaults to 'default'.
+        minsize (int): If the randomly picked image is smaller than minsize, it will
+            be scaled up to this size using Qt.KeepAspectRatio. Defaults to 128.
+        allowDefault (bool): If False is passed do not fallback to the 'default'
+            directory. Defaults to True
+    
+    Returns:
+        QSplashScreen: If a image was picked a QSplashScreen will be returned. Otherwise
+            None will be returned.
+    """
     splash_dir = os.path.join(SPLASH_DIR, toolname)
     # Fallback to the default splashscreens if the requested toolname folder does not exist
-    if not os.path.exists(splash_dir):
+    if allowDefault and not os.path.exists(splash_dir):
         splash_dir = os.path.join(SPLASH_DIR, 'default')
 
     if os.path.isdir(splash_dir):
