eb701b4ad0a977c1847f2da66bac5fc5f963fed7
Mike Hendricks <mikeh@blur.com>
2017-11-14 03:06:25 +0000

    Added blurdev.version.current() and blurdev.version.fromString('v1.10.18787')
    git-svn-id: svn://svn.blur.com/blur/trunk/python/blurdev@18881 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- blurdev/version.py
+++ blurdev/version.py
@@ -2,6 +2,7 @@
 """
 
 import os
+import re
 
 _major = 1  # User defined major version
 _minor = 10  # User defined minor version
@@ -23,8 +24,52 @@ def minor():
     return _minor
 
 
+def current(asDict=False):
+    """ Returns the current version as tuple or dict.
+
+    Args:
+        asDict (bool, optional): Return the value as a dict. If False(the default)
+            a tuple will be returned.
+
+    Returns:
+        tuple or dict: Returns a tuple containing 3 ints (major, minor, build).
+            if asDict is True, a dict is returned.
+    """
+    if asDict:
+        return dict(major=major(), minor=minor(), build=currentBuild())
+    return (major(), minor(), currentBuild())
+
+
 def currentBuild():
     return _currentBuild
+
+
+def fromString(version, asDict=False):
+    """ Converts a string into a tuple or dict of version info.
+
+    Args:
+        version (str): A valid string version. Example: "v01.1.123" or "1.1.123"
+        asDict (bool, optional): Return the value as a dict. If False(the default)
+            a tuple will be returned.
+
+    Returns:
+        tuple or dict: Returns a tuple containing 3 ints (major, minor, build).
+            if asDict is True, a dict is returned.
+
+    Raises:
+        ValueError: A invalid version string was provided.
+    """
+    match = re.match(r'[vV]?(?P<major>\d+)\.(?P<minor>\d+)\.(?P<build>\d+)', version)
+    if match:
+        # Convert to int values
+        ret = match.groupdict()
+        ret['major'] = int(ret['major'])
+        ret['minor'] = int(ret['minor'])
+        ret['build'] = int(ret['build'])
+        if asDict:
+            return ret
+        return (ret['major'], ret['minor'], ret['build'])
+    raise ValueError('Invalid version string: {}'.format(version))
 
 
 def toString(version=None):
