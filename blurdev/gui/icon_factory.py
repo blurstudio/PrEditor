06fd704f5f818e99be60c52e5689ea8921eedf2c
Nathan Mooth <mooth@blur.com>
2019-08-09 18:07:25 -0700

    Added iconfactory for blurdev and the production library.

--- blurdev/gui/icon_factory.py
+++ blurdev/gui/icon_factory.py
@@ -2,11 +2,41 @@ from cute.icon_factory import IconFactory as CuteIconFactory
 
 
 class IconFactory(CuteIconFactory):
+
+    """Subclass that adds finders for the production library's and blurdev's icons. Also loads preset file.
+    """
+
     def __init__(self):
         super(IconFactory, self).__init__()
 
         from pillar.resource_finder import ResourceFinder
         from blurdev import Resources
 
-        finder = ResourceFinder('icons', 'img', parent=Resources())
-        self._configure(finders=finder)
+        from pathlib2 import Path
+
+        finders = []
+        local_path = Path(__file__).parents[0]
+
+        # Build blurdev image finder.
+        root_finder = Resources()
+        bdev_finder = root_finder.child('resources')
+        finders.append(ResourceFinder('blurdev-icons', 'img', parent=bdev_finder))
+
+        # Build library image finder.
+        lib_finder = root_finder.child('library')
+        config_file = local_path / 'library_icons.yaml'
+        if config_file.exists():
+            finders.extend(
+                ResourceFinder.build_from_config_file(
+                    str(config_file), parent=lib_finder
+                )
+            )
+        else:
+            print "No icon config file found."
+            finders.append(ResourceFinder('library-icons', 'icons', parent=lib_finder))
+
+        # Get presets file.
+        presets_file = local_path / 'icon_presets.yaml'
+
+        # Add finders to IconFactory.
+        self._configure(finders=finders, presets=str(presets_file))
--- blurdev/resource_finder.py
+++ blurdev/resource_finder.py
@@ -2,8 +2,21 @@ from pillar.resource_finder import ResourceFinder as PillarResourceFinder
 
 
 class ResourceFinder(PillarResourceFinder):
+
+    """Subclass that adds child finders for the production library and blurdevs own resource folder.
+    """
+
     def __init__(self):
+        super(ResourceFinder, self).__init__('blurdev', '')
+
+        # Build child finder.
+        import os
+
         from pathlib2 import Path
 
+        # Build blurdev resource finder.
         path = Path(__file__).parents[1] / 'resource'
-        super(ResourceFinder, self).__init__('blurdev', path)
+        PillarResourceFinder('resources', str(path), parent=self)
+
+        # Build library finder.
+        PillarResourceFinder('library', os.environ['BDEV_PATH_RESOURCES'], parent=self)
