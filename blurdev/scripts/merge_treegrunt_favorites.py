177da8b99f87c0183e87c39370032dd48edb308a
Mike Hendricks <mikeh@blur.com>
2019-09-05 14:45:56 -0700

    Properly support multiple users in merge_treegrunt_favorites

--- blurdev/scripts/merge_treegrunt_favorites.py
+++ blurdev/scripts/merge_treegrunt_favorites.py
@@ -7,59 +7,91 @@ import getpass
 def getPrefs(limit=None, allUsers=False):
     """ Gather the prefs arguments needed to find old style treegrunt favorites
     """
-    globString = blurdev.prefs.Preference.path(coreName='*')
+    roots = [os.path.dirname(blurdev.prefs.Preference.path())]
     if allUsers:
-        globString = globString.replace(getpass.getuser(), '*')
+        # Handle the migration user folders like magma.0000, etc
+        # user_folder = os.path.split(os.environ['USERPROFILE'])[-1]
+        # globString = roots[0].replace(user_folder, '*')
+        globString = r'C:\Users\*\AppData\Roaming\blur\userprefs'
+        roots = list(set([x for x in set(glob.glob(globString))]))
 
-    rootPath = os.path.dirname(globString)
-    globString = os.path.join(globString, 'treegrunt', '*_favorites.pref')
-    cores = {}
-    for filename in glob.glob(globString):
-        filename = os.path.normpath(filename)
-        relativePath = filename.replace(rootPath + os.path.sep, '')
-        coreName = relativePath.split(os.path.sep)[0].replace('app_', '')
-        path = os.path.splitext(relativePath.split(os.path.sep, 1)[1])[0]
-        if limit and coreName != limit:
-            continue
-        cores.setdefault(coreName, []).append(path)
-    return cores
+    all_prefs = {}
+    for root in roots:
+        globString = os.path.join(root, 'app_*', 'treegrunt', '*_favorites.pref')
+        for filename in glob.glob(globString):
+            relativePath = filename.replace(root + os.path.sep, '')
+            coreName = relativePath.split(os.path.sep)[0].replace('app_', '')
+            path = os.path.splitext(relativePath.split(os.path.sep, 1)[1])[0]
+            if limit and coreName != limit:
+                continue
+            all_prefs.setdefault(root, {}).setdefault(coreName, []).append(path)
+    return all_prefs
 
 
-def getFavorites(name, coreName, favorites):
+def getFavorites(path, root, coreName, favorites):
     """ Extract favorites from a prefs file.
     """
-    prefs = blurdev.prefs.find(name, coreName=coreName, reload=True)
+    prefs = findPref(path, root, coreName)
     children = prefs.root().children()
     for child in children:
         if child.nodeName == 'tool':
             favorites.add(child.attribute('id'))
 
 
-def mergePrefs(cores):
+def mergePrefs(all_prefs):
     """ Combine all the old per treegrunt environment favorites to a single
     favorites file.
     """
-    for coreName in cores:
-        # No need to re-add these favorites
-        existing = set()
-        getFavorites('treegrunt/favorites', coreName, existing)
-        favorites = set()
-        getFavorites('treegrunt/favorites', coreName, favorites)
-        for pref in cores[coreName]:
-            getFavorites(pref, coreName, favorites)
-        # Note: this will make duplicate entries, but this will get fixed when the prefs
-        # are saved normally
-        prefs = blurdev.prefs.find(
-            'treegrunt/favorites', coreName=coreName, reload=True
-        )
-        root = prefs.root()
-        print('Updating: {}'.format(prefs.filename()))
-        for favorite in favorites:
-            if favorite not in existing:
-                print('\tAdding: {}'.format(favorite))
-                node = root.addNode('tool')
-                node.setAttribute('id', favorite)
-        prefs.save()
+    for root in all_prefs:
+        cores = all_prefs[root]
+        txt = '# PROCESSING: {}'.format(root)
+        print(txt)
+        print('#' * len(txt))
+        for coreName in cores:
+            # No need to re-add these favorites
+            existing = set()
+            getFavorites('treegrunt/favorites', root, coreName, existing)
+            favorites = set()
+            getFavorites('treegrunt/favorites', root, coreName, favorites)
+            for pref in cores[coreName]:
+                getFavorites(pref, root, coreName, favorites)
+            # Note: this will make duplicate entries, but this will get fixed when the prefs
+            # are saved normally
+            prefs = findPref(r'treegrunt\favorites', root, coreName)
+            pref_root = prefs.root()
+            changes = []
+            for favorite in favorites:
+                if favorite not in existing:
+                    changes.append('    Adding: {}'.format(favorite))
+                    node = pref_root.addNode('tool')
+                    node.setAttribute('id', favorite)
+            if changes:
+                print('Updating: {}'.format(prefs.filename()))
+                print('\n'.join(changes))
+                prefs.save()
+
+
+def findPref(name, root_path, coreName):
+    """ Get a Preference object for any user given its root_path
+    """
+    key = str(name).replace(' ', '-').lower()
+    filename = os.path.join(root_path, 'app_{}'.format(coreName), '%s.pref' % key)
+    # create a new preference record
+    pref = blurdev.prefs.Preference()
+    pref.setCoreName(coreName)
+    # look for a default preference file
+    success = False
+    pref._filename = filename
+    if os.path.exists(filename):
+        success = pref.load(filename)
+    if not success:
+        # create default information
+        root = pref.addNode('preferences')
+        root.setAttribute('name', name)
+        root.setAttribute('version', 1.0)
+        root.setAttribute('ui', '')
+    pref.setName(key)
+    return pref
 
 
 if __name__ == '__main__':
@@ -85,3 +117,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     prefs = getPrefs(args.limit, allUsers=args.all_users)
     mergePrefs(prefs)
+    print('Finished merging prefs')
