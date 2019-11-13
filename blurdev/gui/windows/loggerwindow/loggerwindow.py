8b2a0077be684541554aaabdd3e2d6eda26ea450
Mike Hendricks <mikeh@blur.com>
2019-11-13 11:48:22 -0800

    Logger only sets the appUserModelId if launched by runtime
    This prevents issues where a treegrunt tool gets parented under the
    logger taskbar icon.

--- blurdev/gui/windows/loggerwindow/loggerwindow.py
+++ blurdev/gui/windows/loggerwindow/loggerwindow.py
@@ -49,7 +49,6 @@ class LoggerWindow(Window):
 
         self.setWindowIcon(QIcon(blurdev.resourcePath('img/PythonLogger.png')))
         blurdev.gui.loadUi(__file__, self)
-        blurdev.setAppUserModelID('PythonLogger')
 
         self.uiConsoleTXT.pdbModeAction = self.uiPdbModeACT
         self.uiConsoleTXT.pdbUpdateVisibility = self.updatePdbVisibility
