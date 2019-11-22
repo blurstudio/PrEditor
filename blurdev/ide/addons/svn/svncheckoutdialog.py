8119ae750f6604f2375088f058f267b89a35eeb4
Mike Hendricks <mikeh@blur.com>
2019-11-22 15:32:11 -0800

    Fix various bugs in files causing warnings in docs generation

--- blurdev/ide/addons/svn/svncheckoutdialog.py
+++ blurdev/ide/addons/svn/svncheckoutdialog.py
@@ -1,89 +1,98 @@
 ##
-#	\namespace	blurdev.ide.addons.svn.svncheckoutdialog
+# 	\namespace	blurdev.ide.addons.svn.svncheckoutdialog
 #
-#	\remarks	
-#	
-#	\author		beta@blur.com
-#	\author		Blur Studio
-#	\date		05/24/11
+# 	\remarks
+#
+# 	\author		beta@blur.com
+# 	\author		Blur Studio
+# 	\date		05/24/11
 #
 
-from Qt.QtWidgets import QFileDialog, QMessageBox, as, msg
+from Qt.QtWidgets import QFileDialog, QMessageBox as msg
 
-from blurdev.gui 			import Dialog
+from blurdev.gui import Dialog
 from blurdev.ide.addons.svn import svnops
-from blurdev.ide.addons.svn	import svnconfig
+from blurdev.ide.addons.svn import svnconfig
 
-class SvnCheckoutDialog( Dialog ):
-    def __init__( self, parent = None ):
-        Dialog.__init__( self, parent )
-        
+
+class SvnCheckoutDialog(Dialog):
+    def __init__(self, parent=None):
+        Dialog.__init__(self, parent)
+
         # load the ui
         import blurdev
-        blurdev.gui.loadUi( __file__, self )
-        
+
+        blurdev.gui.loadUi(__file__, self)
+
         self.uiUrlTXT.setText(svnconfig.CURRENT_URL)
-        
+
         # create connections
-        self.uiUrlBTN.clicked.connect( self.pickUrl )
-        self.uiFilepathBTN.clicked.connect( self.pickPath )
-    
-    def accept( self ):
+        self.uiUrlBTN.clicked.connect(self.pickUrl)
+        self.uiFilepathBTN.clicked.connect(self.pickPath)
+
+    def accept(self):
         # make sure there is a url and a filepath specified
-        if ( not (self.url() and self.filepath()) ):
-            msg.critical(self,'Missing Data', 'You need to provide a URL and filepath for the checkout to work.' )
+        if not (self.url() and self.filepath()):
+            msg.critical(
+                self,
+                'Missing Data',
+                'You need to provide a URL and filepath for the checkout to work.',
+            )
             return False
-        
+
         # create the checkout thread
         from threads.checkoutthread import CheckoutThread
-        
+
         thread = CheckoutThread()
-        thread.setFilepath( self.filepath() )
-        thread.setUrl( self.url() )
-        thread.setRecursive( self.uiRecursiveCHK.isChecked() )
-        thread.setIgnoreExternals( self.uiExternalsCHK.isChecked() )
-        
-        if ( not self.uiHeadCHK.isChecked() ):
+        thread.setFilepath(self.filepath())
+        thread.setUrl(self.url())
+        thread.setRecursive(self.uiRecursiveCHK.isChecked())
+        thread.setIgnoreExternals(self.uiExternalsCHK.isChecked())
+
+        if not self.uiHeadCHK.isChecked():
             print('process a specific revision')
-        
+
         # create the actions dialog
         from svnactiondialog import SvnActionDialog
-        SvnActionDialog.start( self.parent(), thread, title = 'Checkout' )
-        
+
+        SvnActionDialog.start(self.parent(), thread, title='Checkout')
+
         svnconfig.CURRENT_URL = self.url()
-        
+
         # accept the dialog
-        super(SvnCheckoutDialog,self).accept()
-    
-    def filepath( self ):
+        super(SvnCheckoutDialog, self).accept()
+
+    def filepath(self):
         return self.uiFilepathTXT.text()
-    
-    def setFilepath( self, filepath ):
+
+    def setFilepath(self, filepath):
         self.uiFilepathTXT.setText(filepath)
-    
-    def setUrl( self, url ):
+
+    def setUrl(self, url):
         self.uiUrlTXT.setText(url)
-    
-    def pickUrl( self ):
+
+    def pickUrl(self):
         url = svnops.getUrl(str(self.url()))
-        if ( url ):
+        if url:
             self.uiUrlTXT.setText(url)
-    
-    def pickPath( self ):
-        filepath = QFileDialog.getExistingDirectory( self, 'Checkout Directory', self.filePath() )
-        if ( filepath ):
+
+    def pickPath(self):
+        filepath = QFileDialog.getExistingDirectory(
+            self, 'Checkout Directory', self.filePath()
+        )
+        if filepath:
             self.setFilepath(filepath)
-    
-    def url( self ):
+
+    def url(self):
         return str(self.uiUrlTXT.text())
-    
+
     # define static methods
     @staticmethod
-    def checkout( ide, filepath = '', url = '' ):
-        dlg = SvnCheckoutDialog( ide )
+    def checkout(ide, filepath='', url=''):
+        dlg = SvnCheckoutDialog(ide)
         dlg.setFilepath(filepath)
-        
-        if ( url ):
+
+        if url:
             dlg.setUrl(url)
-            
+
         dlg.show()
