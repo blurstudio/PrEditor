3c24c896361385f7ab2764e90b5182ef497e4f3f
Erik Carlson <carlson@blur.com>
2020-01-15 12:27:46 -0800

    Add support for offline and default settings.ini values
    #46958

--- blurdev/settings.py
+++ blurdev/settings.py
@@ -24,6 +24,15 @@ elif os.name == 'nt':
 elif os.name == 'osx':
     OS_TYPE = 'MacOS'
 
+# The sections to add from settings.ini
+# The order matters. Add from most specific to least specific.
+# Example: Add Windows Offline, then Windows, then Default.
+# Environment variables that exist in os.environ will not be added.
+_SECTIONS_TO_ADD = []
+if os.getenv('BDEV_OFFLINE') == '1':
+    _SECTIONS_TO_ADD.append('{} Offline'.format(OS_TYPE))
+_SECTIONS_TO_ADD += [OS_TYPE, 'Default']
+
 _currentEnv = ''
 _inited = False
 defaults = {}
@@ -37,17 +46,33 @@ def environStr(value):
     return value.encode('utf8')
 
 
+def addConfigSection(config_parser, section):
+    """
+    Add a config section to os.environ for a section.
+
+    Does not add options that already exist in os.environ.
+
+    Args:
+        config_parser (configparser.RawConfigParser): The parser to read from.
+            Must already be read.
+        section (str): The section name to add.
+    """
+
+    for option in config_parser.options(section):
+        if option not in os.environ:
+            value = config_parser.get(section, option)
+            if value == 'None':
+                value = ''
+            # In python2.7 on windows you can't pass unicode values to subprocess.Popen's env argument.
+            # This is the reason we are calling str()
+            os.environ[environStr(option.upper())] = environStr(value)
+
+
 # load the default environment from the settings INI
 config = configparser.RawConfigParser()
 config.read(os.path.dirname(__file__) + '/resource/settings.ini')
-for option in config.options(OS_TYPE):
-    if option not in os.environ:
-        value = config.get(OS_TYPE, option)
-        if value == 'None':
-            value = ''
-        # In python2.7 on windows you can't pass unicode values to subprocess.Popen's env argument.
-        # This is the reason we are calling str()
-        os.environ[environStr(option.upper())] = environStr(value)
+for section in _SECTIONS_TO_ADD:
+    addConfigSection(config, section)
 
 # store the blurdev path in the environment
 os.environ['BDEV_PATH'] = environStr(os.path.dirname(__file__))
