5548370df96ce0bc7d25b6ea30bf107f238bbb53
Mike Hendricks <mikeh@blur.com>
2020-08-27 11:09:43 -0700

    Add environment activation cli commands

--- blurdev/cli/commands/cmd_env.py
+++ blurdev/cli/commands/cmd_env.py
@@ -10,6 +10,45 @@ def cli():
 
 
 @cli.command()
+@click.argument('env')
+@click.option(
+    '-c',
+    '--core-name',
+    default='external',
+    help="Set the active environment for this core name.",
+)
+def activate(env, core_name):
+    """ Change the currently selected treegrunt environment """
+
+    success = blurdev.activeEnvironment(coreName=core_name) == env
+    if not success:
+        # This returns false if env is already active
+        success = blurdev.setActiveEnvironment(env, coreName=core_name)
+
+    if success:
+        msg = 'Set active environment: {env} for core name: {core}'
+    else:
+        msg = 'Unable to set the active environment {env} for core name {core}'
+    click.echo(msg.format(env=env, core=core_name))
+
+
+@cli.command()
+@click.option(
+    '-c',
+    '--core-name',
+    default='external',
+    help="Get the currently active environment for this core name.",
+)
+def active(core_name):
+    """ Returns the currently selected treegrunt environment """
+    click.echo(
+        'Active Environment: {} for core name: {}'.format(
+            blurdev.activeEnvironment(coreName=core_name).objectName(), core_name
+        )
+    )
+
+
+@cli.command()
 @click.option(
     '-n', '--name', help="Use this treegrunt environment instead of the active one."
 )
