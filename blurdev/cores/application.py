441c730502dc9243f4fbd500e66c42cffb7b0221
Mike Hendricks <mikeh@blur.com>
2021-07-08 14:37:36 -0500

    Make Qt automatically scale based on the monitor the window is on

--- blurdev/cores/application.py
+++ blurdev/cores/application.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
+import sys
+import Qt
 from Qt.QtWidgets import QApplication
 from Qt.QtCore import QCoreApplication, Signal
 
 
 class CoreApplication(QCoreApplication):
-    # Does not get called, but exists incase a script tries to connect to it.
+    # Does not get called, but exists in case a script tries to connect to it.
     sessionEnding = Signal()
 
 
@@ -12,6 +14,10 @@ class Application(QApplication):
     sessionEnding = Signal()
 
     def __init__(self, args):
+        if sys.platform == 'win32' and Qt.IsPyQt5:
+            # Make Qt automatically scale based on the monitor the window is
+            # currently located.
+            args.extend(["--platform", "windows:dpiawareness=0"])
         super(Application, self).__init__(args)
 
     def commitData(self, sessionManager):
