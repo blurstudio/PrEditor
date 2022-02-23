525e004b0f77a5ead75ad7d8b22151d53ab00e25
Mike Hendricks <mikeh@blur.com>
2022-02-23 11:35:05 -0800

    Add local caching to the blurdev ResourceFinder

--- blurdev/resource_finder.py
+++ blurdev/resource_finder.py
@@ -1,4 +1,8 @@
 from __future__ import absolute_import
+import os
+
+from pathlib2 import Path
+
 from pillar.resource_finder import ResourceFinder as PillarResourceFinder
 
 
@@ -9,16 +13,23 @@ class ResourceFinder(PillarResourceFinder):
     """
 
     def __init__(self):
-        super(ResourceFinder, self).__init__('blurdev', '')
+        # Set the resource cache for local resource caching.
+        cache = os.getenv('BDEV_RESOURCES_CACHE')
+        if cache:
+            cache_path = Path(cache)
+            cache_path = cache_path / 'blurdev'
+            cache = str(cache_path)
 
-        # Build child finder.
-        import os
-
-        from pathlib2 import Path
+        super(ResourceFinder, self).__init__('blurdev', '', cache_paths=cache)
 
         # Build blurdev resource finder.
         path = Path(__file__).parents[0] / 'resource'
         PillarResourceFinder('resources', str(path), parent=self)
 
         # Build library finder.
-        PillarResourceFinder('library', os.environ['BDEV_PATH_RESOURCES'], parent=self)
+        PillarResourceFinder(
+            'library',
+            os.getenv('BDEV_PATH_RESOURCES', ''),
+            cache_paths="library",
+            parent=self,
+        )
