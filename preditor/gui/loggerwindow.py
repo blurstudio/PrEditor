fc780eb9bf7f970cc6c91f52ae4fc6e8432231bf
Mark Mancewicz <markm@blur.com>
2025-08-28 09:05:14 -0700

    Optionally confirm before closing with Ctrl+Q

--- preditor/gui/loggerwindow.py
+++ preditor/gui/loggerwindow.py
@@ -139,7 +139,7 @@ class LoggerWindow(Window):
         self.uiLogToFileClearACT.setVisible(False)
 
         self.uiRestartACT.triggered.connect(self.restartLogger)
-        self.uiCloseLoggerACT.triggered.connect(self.closeLogger)
+        self.uiCloseLoggerACT.triggered.connect(self.closeLoggerByAction)
 
         self.uiRunAllACT.triggered.connect(self.execAll)
         # Even though the RunSelected actions (with shortcuts) are connected
@@ -1134,7 +1134,17 @@ class LoggerWindow(Window):
         for editor, _, _, _, _ in self.uiWorkboxTAB.all_widgets():
             editor.__close__()
 
-    def closeLogger(self):
+    def closeLoggerByAction(self):
+        if self.uiConfirmBeforeCloseCHK.isChecked():
+            msg = "Are you sure you want to close PrEditor?"
+            ret = QMessageBox.question(
+                self,
+                'Confirm close',
+                msg,
+                QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.Cancel,
+            )
+            if ret != QMessageBox.StandardButton.Yes:
+                return
         self.close()
 
     def execAll(self):
@@ -1227,6 +1237,7 @@ class LoggerWindow(Window):
                 ),
                 'dont_ask_again': self.dont_ask_again,
                 'max_num_backups': self.uiMaxNumBackupsSPIN.value(),
+                'confirmBeforeClose': self.uiConfirmBeforeCloseCHK.isChecked(),
             }
         )
 
@@ -1501,6 +1512,9 @@ class LoggerWindow(Window):
         workboxHintingEnabled = pref.get('workboxHintingEnabled', True)
         self.uiWorkboxAutoCompleteEnabledCHK.setChecked(workboxHintingEnabled)
         self.setAutoCompleteEnabled(workboxHintingEnabled, console=False)
+
+        confirmBeforeClose = pref.get('confirmBeforeClose', True)
+        self.uiConfirmBeforeCloseCHK.setChecked(confirmBeforeClose)
 
         # Ensure the correct workbox stack page is shown
         self.update_workbox_stack()
