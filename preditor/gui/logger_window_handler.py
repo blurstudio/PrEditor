68cc1ddd80554fdfcfcdd03cf39aa1551773d912
Mike Hendricks <mikeh@blur.com>
2025-11-03 11:44:08 -0800

    LoggerWindowHandler can now force the console to print
    - This allows them to write their messages even if they wouldn't normally.
    - Also remove legacy unused outputPipe. Instead add a callback to preditor.active.

--- preditor/gui/logger_window_handler.py
+++ preditor/gui/logger_window_handler.py
@@ -3,46 +3,72 @@ from __future__ import absolute_import
 import logging
 
 from .. import instance
+from ..constants import StreamType
 
 
 class LoggerWindowHandler(logging.Handler):
     """A logging handler that writes directly to the PrEditor instance.
 
     Args:
-        error (bool, optional): Write the output as if it were written
-            to sys.stderr and not sys.stdout. Ie in red text.
         formatter (str or logging.Formatter, optional): If specified,
             this is passed to setFormatter.
+        stream (optional): If provided write to this stream instead of the
+            main preditor instance's console.
+        stream_type (StreamType, optional): If not None, pass this value to the
+            write call's force kwarg.
     """
 
     default_format = (
         '%(levelname)s %(module)s.%(funcName)s line:%(lineno)d - %(message)s'
     )
 
-    def __init__(self, error=True, formatter=default_format):
+    def __init__(
+        self,
+        formatter=default_format,
+        stream=None,
+        stream_type=StreamType.STDERR | StreamType.CONSOLE,
+    ):
         super(LoggerWindowHandler, self).__init__()
-        self.error = error
+        self.stream_type = stream_type
+        self.stream = stream
+
         if formatter is not None:
             if not isinstance(formatter, logging.Formatter):
                 formatter = logging.Formatter(formatter)
             self.setFormatter(formatter)
 
     def emit(self, record):
-        _instance = instance(create=False)
-        if _instance is None:
-            # No gui has been created yet, so nothing to do
-            return
         try:
-            # If the python logger was closed and garbage collected,
-            # there is nothing to do, simply exit the call
-            console = _instance.console()
-            if not console:
+            # If no gui has been created yet, or the `preditor.instance()` was
+            # closed and garbage collected, there is nothing to do, simply exit
+            stream = self.stream
+            if not stream:
                 return
-
             msg = self.format(record)
-            msg = u'{}\n'.format(msg)
-            console.write(msg, self.error)
+            kwargs = {}
+            if self.stream_type is not None:
+                kwargs["stream_type"] = self.stream_type
+            stream.write(f'{msg}\n', **kwargs)
         except (KeyboardInterrupt, SystemExit):
             raise
         except Exception:
             self.handleError(record)
+
+    @property
+    def stream(self):
+        """The stream to write log messages to.
+
+        If no stream is set then it returns `preditor.instance().console()`.
+        """
+        if self._stream is not None:
+            return self._stream
+
+        _instance = instance(create=False)
+        if _instance is None:
+            return None
+        # Note: This does not handle if the instance has been closed
+        return _instance.console()
+
+    @stream.setter
+    def stream(self, stream):
+        self._stream = stream
