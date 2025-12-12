1c3848d29bb82d65c82fc0824bff4bea566ad8e8
Mike Hendricks <mikeh@blur.com>
2025-12-11 16:46:15 -0800

    The OutputConsole now use the same stylesheet used by preditor
    Updating the style of the controller will automatically apply that style
    to the OutputWindow. This is not enabled by default for ConsoleBase or ConsolePrEdit.

--- preditor/gui/output_console.py
+++ preditor/gui/output_console.py
@@ -1,5 +1,11 @@
+from ..utils.cute import QtPropertyInit
 from .console_base import ConsoleBase
 
 
 class OutputConsole(ConsoleBase):
     """A text widget used to show stdout/stderr writes."""
+
+    # Enable these settings by default
+    use_console_stylesheet = QtPropertyInit(
+        "_use_console_stylesheet", True, callback=ConsoleBase.init_stylesheet
+    )
